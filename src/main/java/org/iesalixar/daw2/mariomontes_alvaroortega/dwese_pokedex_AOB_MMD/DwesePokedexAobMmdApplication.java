ef443d319056d7d9dcd07ea8123d966f39c88a17
Arvarisu <alvaroortegabenite03@gmail.com>
2025-10-27 16:04:55 +0100

    Commit del esqueleto inicial del proyecto

---
+++ src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/DwesePokedexAobMmdApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.mariomontes_alvaroortega.dwese_pokedex_AOB_MMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwesePokedexAobMmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwesePokedexAobMmdApplication.class, args);
+	}
+
+}
