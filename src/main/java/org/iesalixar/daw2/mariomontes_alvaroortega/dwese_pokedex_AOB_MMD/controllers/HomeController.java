4c534fe96d820e7cbb4c86f6e7d2eadc21095c20
Arvarisu <alvaroortegabenite03@gmail.com>
2025-10-28 16:52:22 +0100

    Configuracion de HomeController para redireccion a la pagina principal

--- src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/controllers/HomeController.java
+++ src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package org.iesalixar.daw2.mariomontes_alvaroortega.dwese_pokedex_AOB_MMD.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
 }
