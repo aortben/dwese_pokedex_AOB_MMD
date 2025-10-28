3e526070f0b4f719ff89dc1fe2ca43148792591e
Arvarisu <alvaroortegabenite03@gmail.com>
2025-10-28 16:33:06 +0100

    Entidad pokemon configurada

--- src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/entities/Pokemon.java
+++ src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/entities/Pokemon.java
@@ -1,4 +1,25 @@
 package org.iesalixar.daw2.mariomontes_alvaroortega.dwese_pokedex_AOB_MMD.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private int level;
+
 }
