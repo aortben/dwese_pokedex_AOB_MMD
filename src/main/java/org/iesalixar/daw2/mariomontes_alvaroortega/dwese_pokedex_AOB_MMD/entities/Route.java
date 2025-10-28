0672c2c3f076d957c54d3b5b1c3622e02f293f46
MMD1358 <xdeadx1358@gmail.com>
2025-10-28 16:45:30 +0100

    Rutas y Regiones v2

--- src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/entities/Route.java
+++ src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/entities/Route.java
@@ -3,13 +3,21 @@ package org.iesalixar.daw2.mariomontes_alvaroortega.dwese_pokedex_AOB_MMD.entiti
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 
 public class Route {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    public Route(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
