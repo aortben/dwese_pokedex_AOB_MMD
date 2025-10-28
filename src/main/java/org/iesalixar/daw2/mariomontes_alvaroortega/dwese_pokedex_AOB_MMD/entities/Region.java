b3b3ff7ac69b6dc9c93a3f85e06637784094c30c
MMD1358 <xdeadx1358@gmail.com>
2025-10-28 16:30:55 +0100

    region y rutas

--- src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/entities/Region.java
+++ src/main/java/org/iesalixar/daw2/mariomontes_alvaroortega/dwese_pokedex_AOB_MMD/entities/Region.java
@@ -1,4 +1,15 @@
 package org.iesalixar.daw2.mariomontes_alvaroortega.dwese_pokedex_AOB_MMD.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class Region {
+    private Long id;
+    private String name;
+
+    public Region(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
